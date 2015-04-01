79af75939dc1903e498d3f7b2a0e4ab158479ac8
Andreas Pohl <apohl79@gmail.com>
2015-04-01 20:54:11 +0200

    RPi sound: limit the max number of player instances per file

--- rpi_sound.sh
+++ rpi_sound.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+num_running=$(ps -ef|grep omxplayer|grep $1|wc -l)
+if [ $num_running -gt 2 ]; then
+    exit;
+fi
 export LD_LIBRARY_PATH=/opt/vc/lib:/usr/lib/omxplayer
 /usr/bin/omxplayer.bin -o both $1 &
 pid=$!
